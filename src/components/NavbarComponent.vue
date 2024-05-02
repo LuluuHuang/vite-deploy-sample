dbf3f9119625d840eba950ef2bfbec2ea927c337
Lulu <yuruh18@gmail.com>
2024-05-02 17:59:56 +0800

    0502

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">餅乾店</span>
+            <butto type="button" class="btn">
+            購物車
+            <span class="badge rounded-pill bg-danger">{{cart.length}}</span>
+            </butto>
+        </div>
+    </nav>
+</template>
+<script>
+    import { mapState } from "pinia";
+    import cartStore from "../stores/cartStore.js";
+    export default {
+        template: `
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">餅乾店</span>
+                    <butto type="button" class="btn">
+                    購物車
+                    <span class="badge rounded-pill bg-danger">{{cart.length}}</span>
+                    </butto>
+                </div>
+            </nav>
+        `,
+        computed:{
+            ...mapState(cartStore,['cart'])
+        }
+    };
+</script>
