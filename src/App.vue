72cecceea56adc8f022ffd0e64131190cfd4b23d
Lulu <yuruh18@gmail.com>
2024-05-03 15:00:40 +0800

    0503

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script>
   import NavbarComponent from './components/NavbarComponent.vue';
   import ProductsComponent from './components/ProductsComponent.vue';
+  import CartComponent from './components/CartComponent.vue';
+
   export default{
     components:{
       NavbarComponent,
-      ProductsComponent
+      ProductsComponent,
+      CartComponent
     }
   }
 </script>
@@ -12,10 +15,20 @@
 <template>
   <div class="container">
     <NavbarComponent></NavbarComponent>
+    <CartComponent></CartComponent>
     <ProductsComponent></ProductsComponent>
   </div>
 </template>
 
 <style lang="scss">
   @import"./assets/all.scss";
+  .card-img-top {
+    object-fit: cover;
+    height: 200px;
+  }
+  .table-image {
+    width: 75px;
+    height: 75px;
+    object-fit: cover;
+  }
 </style>
